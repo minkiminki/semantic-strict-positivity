261c2d2858258f098435a21b7978b544b21c0641
Jeehoon Kang <jeehoon.kang@sf.snu.ac.kr>
2017-07-12 02:32:40 +0900

    WIP5

--- Category.v
+++ Category.v
@@ -5,9 +5,10 @@ Set Implicit Arguments.
 Set Automatic Coercions Import.
 
 
-(* Tactics.  MOVE IT! *)
+(* Tactics.  FIXME: move it! *)
 
 Ltac inv H := inversion H; subst; clear H.
+
 
 (* Categories *)
 
@@ -51,6 +52,7 @@ Notation functorType := Functor.type.
 Notation FunctorType := Functor.pack.
 Definition functor_map F := Functor.map (Functor.class F).
 Notation "'fmap' f" := (@functor_map _ _ _ f) (at level 0).
+Hint Unfold functor_map.
 
 
 Module NatTrans.
@@ -124,6 +126,8 @@ Definition functor_mem F := PFunctor.mem (PFunctor.class F).
 Definition functor_rel F := PFunctor.rel (PFunctor.class F).
 Notation "'fmem' fx" := (@functor_mem _ _ fx) (at level 0).
 Notation "'frel' rel" := (@functor_rel _ _ _ rel) (at level 0).
+Hint Unfold functor_mem.
+Hint Unfold functor_rel.
 
 
 Module PNatTrans.
@@ -194,31 +198,31 @@ Notation "p <3= q" :=
 (* Instances *)
 
 Program Definition id_functorMixin :=
-  Eval hnf in @Functor.Mixin id (fun _ _ => id) _ _.
+  @Functor.Mixin id (fun _ _ => id) _ _.
 Canonical Structure id_functorType := FunctorType id_functorMixin.
 
 Program Definition id_pFunctorMixin :=
-  Eval hnf in
-    @PFunctor.Mixin
-      id id_functorMixin.(Functor.map)
-      (fun _ fx x => fx = x)
-      (fun _ _ rel fx fy => rel fx fy)
-      _.
-Canonical Structure id_pFunctorType := Eval hnf in PFunctorType _ id_pFunctorMixin.
+  @PFunctor.Mixin
+    id id_functorMixin.(Functor.map)
+    (fun _ fx x => fx = x)
+    (fun _ _ rel fx fy => rel fx fy)
+    _.
+Canonical Structure id_pFunctorType := PFunctorType _ id_pFunctorMixin.
+
+Hint Unfold id.
 
 
 Program Definition const_functorMixin T :=
-  Eval hnf in @Functor.Mixin (fun _ => T) (fun _ _ _ => id) _ _.
-Canonical Structure const_functorType T := Eval hnf in FunctorType (const_functorMixin T).
+  @Functor.Mixin (fun _ => T) (fun _ _ _ => id) _ _.
+Canonical Structure const_functorType T := FunctorType (const_functorMixin T).
 
 Program Definition const_pFunctorMixin T :=
-  Eval hnf in
-    @PFunctor.Mixin
-      (fun _ => T) (const_functorMixin T).(Functor.map)
-      (fun _ _ _ => False)
-      (fun _ _ _ => eq)
-      _.
-Program Canonical Structure const_pFunctorType (T:Type) := Eval hnf in PFunctorType (FunctorType (const_functorMixin T)) (const_pFunctorMixin T).
+  @PFunctor.Mixin
+    (fun _ => T) (const_functorMixin T).(Functor.map)
+    (fun _ _ _ => False)
+    (fun _ _ _ => eq)
+    _.
+Program Canonical Structure const_pFunctorType (T:Type) := PFunctorType (FunctorType (const_functorMixin T)) (const_pFunctorMixin T).
 
 
 Definition function_map D (F: functorType) T1 T2 (f: T1 -> T2) (fx1: D -> F T1) :=
@@ -233,7 +237,7 @@ Definition function_rel D (F: pFunctorType) T1 T2
   forall d, frel f (fx1 d) (fx2 d).
 
 Program Definition function_functorMixin D (F: functorType) :=
-  Eval hnf in @Functor.Mixin (fun T => D -> F T) (@function_map _ _) _ _.
+  @Functor.Mixin (fun T => D -> F T) (@function_map _ _) _ _.
 Next Obligation.
   apply functional_extensionality. intro.
   apply functional_extensionality. intro.
@@ -246,14 +250,16 @@ Qed.
 Canonical Structure function_functorType D F := FunctorType (function_functorMixin D F).
 
 Program Definition function_pFunctorMixin D (F: pFunctorType) :=
-  Eval hnf in
-    @PFunctor.Mixin (fun T => D -> F T) (@function_map _ _)
-                    (@function_mem _ _) (@function_rel _ _) _.
+  @PFunctor.Mixin (fun T => D -> F T) (@function_map _ _)
+                  (@function_mem _ _) (@function_rel _ _) _.
 Next Obligation.
   inversion MEM. econstructor.
   apply PFunctor.MEM. eauto.
 Qed.
-Canonical Structure function_pFunctorType D (F: pFunctorType) := Eval hnf in PFunctorType (FunctorType (function_functorMixin D F)) (function_pFunctorMixin D F).
+Canonical Structure function_pFunctorType D (F: pFunctorType) := PFunctorType (FunctorType (function_functorMixin D F)) (function_pFunctorMixin D F).
+
+Hint Unfold function_map.
+Hint Unfold function_rel.
 
 
 Program Definition option_functorMixin :=
@@ -276,13 +282,12 @@ Inductive option_frel X Y (rel: forall (x:X) (y:Y), Prop):
 .
 
 Program Definition option_pFunctorMixin :=
-  Eval hnf in
-    @PFunctor.Mixin
-      option option_functorMixin.(Functor.map)
-      (fun _ fx x => fx = Some x)
-      option_frel
-      _.
-Canonical Structure option_pFunctorType := Eval hnf in PFunctorType _ option_pFunctorMixin.
+  @PFunctor.Mixin
+    option option_functorMixin.(Functor.map)
+    (fun _ fx x => fx = Some x)
+    option_frel
+    _.
+Canonical Structure option_pFunctorType := PFunctorType _ option_pFunctorMixin.
 
 
 Definition coproduct_type (F1 F2: Type -> Type) T := (F1 T + F2 T)%type.
@@ -308,7 +313,7 @@ Inductive coproduct_rel (F1 F2: pFunctorType) T1 T2 f:
 .
 
 Program Definition coproduct_functorMixin (F1 F2: functorType) :=
-  Eval hnf in @Functor.Mixin (coproduct_type F1 F2) (coproduct_map F1 F2) _ _.
+  @Functor.Mixin (coproduct_type F1 F2) (coproduct_map F1 F2) _ _.
 Next Obligation.
   apply functional_extensionality. intro.
   destruct x; simpl.
@@ -320,41 +325,42 @@ Next Obligation.
   - f_equal. apply Functor.MAP_COMPOSE.
   - f_equal. apply Functor.MAP_COMPOSE.
 Qed.
-Canonical Structure coproduct_functorType F1 F2 := Eval hnf in FunctorType (coproduct_functorMixin F1 F2).
+Canonical Structure coproduct_functorType F1 F2 := FunctorType (coproduct_functorMixin F1 F2).
 
 Program Definition coproduct_pFunctorMixin (F1 F2: pFunctorType) :=
-  Eval hnf in
-    @PFunctor.Mixin (coproduct_type F1 F2) (coproduct_map F1 F2)
-                    (@coproduct_mem F1 F2) (@coproduct_rel F1 F2) _.
+  @PFunctor.Mixin (coproduct_type F1 F2) (coproduct_map F1 F2)
+                  (@coproduct_mem F1 F2) (@coproduct_rel F1 F2) _.
 Next Obligation.
   destruct fx; simpl in *.
   - apply PFunctor.MEM. auto.
   - apply PFunctor.MEM. auto.
 Qed.
-Canonical Structure coproduct_pFunctorType F1 F2 := Eval hnf in PFunctorType _ (coproduct_pFunctorMixin F1 F2).
+Canonical Structure coproduct_pFunctorType F1 F2 := PFunctorType _ (coproduct_pFunctorMixin F1 F2).
+
+Hint Unfold coproduct_map.
+Hint Unfold coproduct_mem.
 
 
 Program Definition compose_functorMixin (F1 F2: functorType) :=
-  Eval hnf in @Functor.Mixin (F2 ∘ F1) (fun _ _ f => fmap (fmap f)) _ _.
+  @Functor.Mixin (F2 ∘ F1) (fun _ _ f => fmap (fmap f)) _ _.
 Next Obligation.
   apply functional_extensionality. intro.
   unfold functor_map. rewrite ? Functor.MAP_ID. auto.
 Qed.
 Next Obligation.
 Admitted.
-Canonical Structure compose_functorType F1 F2 := Eval hnf in FunctorType (compose_functorMixin F1 F2).
+Canonical Structure compose_functorType F1 F2 := FunctorType (compose_functorMixin F1 F2).
 
 Program Definition compose_pFunctorMixin (F1 F2: pFunctorType) :=
-  Eval hnf in
-    @PFunctor.Mixin
-      (F2 ∘ F1) (compose_functorMixin F1 F2).(Functor.map)
-      _
-      _
-      _.
+  @PFunctor.Mixin
+    (F2 ∘ F1) (compose_functorMixin F1 F2).(Functor.map)
+    _
+    _
+    _.
 Next Obligation.
 Admitted.
 Next Obligation.
 Admitted.
 Next Obligation.
 Admitted.
-Canonical Structure compose_pFunctorType F1 F2 := Eval hnf in PFunctorType _ (compose_pFunctorMixin F1 F2).
+Canonical Structure compose_pFunctorType F1 F2 := PFunctorType _ (compose_pFunctorMixin F1 F2).
--- PositiveFunctor.v
+++ PositiveFunctor.v
@@ -14,18 +14,20 @@ Section UniversalFunctor.
 
   Definition UF T := Sh1 -> T + Sh2.
 
-   Definition UF_functorMixin :=
-     Eval hnf in function_functorMixin Sh1 (coproduct_functorType id_functorType (const_functorType Sh2)).
-   Definition UF_pFunctorMixin :=
-     Eval hnf in function_pFunctorMixin Sh1 (coproduct_pFunctorType id_pFunctorType (const_pFunctorType Sh2)).
+   Definition UF_functorMixin: Functor.mixin_of UF :=
+     function_functorMixin Sh1 (coproduct_functorType id_functorType (const_functorType Sh2)).
+   Definition UF_pFunctorMixin: PFunctor.mixin_of UF UF_functorMixin.(Functor.map) :=
+     function_pFunctorMixin Sh1 (coproduct_pFunctorType id_pFunctorType (const_pFunctorType Sh2)).
 End UniversalFunctor.
 
-Canonical Structure UF_FunctorType Sh1 Sh2 := Eval hnf in FunctorType (UF_functorMixin Sh1 Sh2).
-Canonical Structure UF_pFunctorType Sh1 Sh2 := Eval hnf in PFunctorType (UF_FunctorType Sh1 Sh2) (UF_pFunctorMixin Sh1 Sh2).
+Canonical Structure UF_FunctorType Sh1 Sh2 := FunctorType (UF_functorMixin Sh1 Sh2).
+Canonical Structure UF_PFunctorType Sh1 Sh2 := PFunctorType (UF_FunctorType Sh1 Sh2) (UF_pFunctorMixin Sh1 Sh2).
 
+Hint Unfold UF_FunctorType.
+Hint Unfold UF_PFunctorType.
 
 Module PositiveFunctor.
-  Record mixin_of (F: Type -> Type) (F_map:forall T1 T2 (f: forall (x1:T1), T2) (fx1:F T1), F T2): Type := Mixin {
+  Program Record mixin_of (F: Type -> Type) (F_map:forall T1 T2 (f: forall (x1:T1), T2) (fx1:F T1), F T2): Type := Mixin {
     Sh1: Type;
     Sh2: Type;
     embedding: forall T (x: F T), UF Sh1 Sh2 T;
@@ -33,7 +35,7 @@ Module PositiveFunctor.
     INJECTIVE: forall T x1 x2 (EQ: @embedding T x1 = @embedding T x2), x1 = x2;
     NATURAL:
       forall T1 T2 (f: T1 -> T2) fx1,
-        embedding (F_map _ _ f fx1) = functor_map (UF_FunctorType _ _) f (embedding fx1);
+        embedding (F_map _ _ f fx1) = fmap f (embedding fx1);
   }.
 
   Record class_of (F: Type -> Type): Type := Class {
@@ -66,6 +68,7 @@ Canonical Structure PositiveFunctor.pFunctorType.
 Canonical Structure PositiveFunctor.functorType.
 Definition functor_embedding F := PositiveFunctor.embedding (PositiveFunctor.class F).
 Notation "'femb' fx" := (@functor_embedding _ _ fx) (at level 0).
+Hint Unfold functor_embedding.
 
 
 (* Theory *)
@@ -80,7 +83,7 @@ Section MFix.
   Inductive range: forall (u:ufix), Prop :=
   | Range
       (m: PF ufix)
-      (OnTL: functor_mem (UF_pFunctorType _ _) (femb m) <1= range):
+      (OnTL: fmem (femb m) <1= range):
       range (Ufix (femb m))
   .
 
@@ -89,15 +92,6 @@ Section MFix.
   Definition mfix_to_ufix (m:mfix): ufix := proj1_sig m.
 
   (* FIXME: move *)
-  Hint Unfold coproduct_map.
-  Hint Unfold coproduct_mem.
-  Hint Unfold function_map.
-  Hint Unfold function_rel.
-  Hint Unfold functor_map.
-  Hint Unfold functor_mem.
-  Hint Unfold functor_rel.
-  Hint Unfold functor_embedding.
-  Hint Unfold id.
 
   Program Definition Mfix (m: PF mfix) : mfix :=
     @exist _ _ (Ufix (femb (fmap mfix_to_ufix m))) _.
@@ -119,7 +113,7 @@ Section MFix.
   (*   end. *)
   
   Inductive ufix_order: forall (x y:ufix), Prop :=
-  | Ufix_order x u (IN: functor_mem (UF_pFunctorType _ _) u x): ufix_order x (Ufix u)
+  | Ufix_order x u (IN: fmem u x): ufix_order x (Ufix u)
   .
 
   Lemma ufix_order_wf: well_founded ufix_order.
@@ -127,14 +121,12 @@ Section MFix.
     unfold well_founded. fix 1. intro. destruct a.
     constructor. intros.
     inv H. inversion IN.
-    unfold functor_mem in MEM. simpl in *.
-    unfold coproduct_mem in MEM. simpl in *.
+    repeat (autounfold in *; simpl in *).
     destruct (u d).
-    - unfold functor_mem in MEM. simpl in *.
-      specialize (ufix_order_wf u0).
+    - specialize (ufix_order_wf u0).
       rewrite MEM in ufix_order_wf.
       apply ufix_order_wf.
-    - unfold functor_mem in MEM. simpl in *. inversion MEM.
+    - inv MEM.
   Defined.
 
   Inductive mfix_order: forall (x y:mfix), Prop :=
@@ -144,7 +136,7 @@ Section MFix.
   Lemma mfix_order_ufix_order x y (ORD: mfix_order x y):
     ufix_order (mfix_to_ufix x) (mfix_to_ufix y).
   Proof.
-    inversion ORD. auto.
+    inv ORD. auto.
   Qed.
 End MFix.
 
@@ -158,7 +150,7 @@ Program Definition id_positiveFunctorMixin :=
     (fun _ x _ => inl x)
     _ _.
 Next Obligation.
-  eapply fapp in EQ; [|apply ()]. inversion EQ. auto.
+  eapply fapp in EQ; [|apply ()]. inv EQ. auto.
 Qed.
 Canonical Structure id_positiveFunctorType := PositiveFunctorType _ id_positiveFunctorMixin.
 
@@ -175,9 +167,9 @@ Program Definition option_positiveFunctorMixin :=
     _ _.
 Next Obligation.
   destruct x1, x2; simpl in *; auto.
-  - eapply fapp in EQ; [|apply ()]. inversion EQ. auto.
-  - eapply fapp in EQ; [|apply ()]. inversion EQ.
-  - eapply fapp in EQ; [|apply ()]. inversion EQ.
+  - eapply fapp in EQ; [|apply ()]. inv EQ. auto.
+  - eapply fapp in EQ; [|apply ()]. inv EQ.
+  - eapply fapp in EQ; [|apply ()]. inv EQ.
 Qed.
 Next Obligation.
   destruct fx1; auto.
